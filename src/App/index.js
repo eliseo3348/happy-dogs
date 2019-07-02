0df56946bcfcb267b118d2b089e934430ce2be2c
eliseo3348 <carloseliseo.fuentes.soto@gmail.com>
2019-07-02 17:47:56 -0400

    Creando mini perfil

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Profile from './Profile'
+import Profile from './components/Profile'
+import Mini_Profile from './components/Mini_Profile'
 
 function App() {
   return (
-    <Profile/>
+    //<Profile/>
+    <Mini_Profile/>
   );
 }
 
